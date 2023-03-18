514f1130a73305502a882925177f8310ecee69fd
Tiago09YT <sanyara2010@gmail.com>
2023-03-17 19:40:00 -0500

    Ejercicio2

---
+++ Modulo2/Ejercicio.js
@@ -0,0 +1,12 @@
+let nombre = "Santiago";
+let edad = 22;
+let isDeveloper = true;
+let birthDay = new Date(2000,8,2);
+const favBook = {
+    titulo: "El silencio de la ciudad blanca",
+    autor: "Eva García Sáenz de Urturi",
+    fecha: new Date(2016,3),
+    url: "https://www.planetadelibros.com/libro-el-silencio-de-la-ciudad-blanca/211749"
+}
+
+let lista = [nombre, edad, isDeveloper, birthDay, favBook]
