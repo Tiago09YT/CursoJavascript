b5c68b891f837cf2c47ad3f0503a67ae1f26a065
Tiago09YT <sanyara2010@gmail.com>
2023-03-21 14:48:21 -0500

    Ejercicio5

---
+++ Modulo5/Ejercicio.js
@@ -0,0 +1,6 @@
+let altura_cm = 180;
+let altura_m = 1.8;
+let peso_kg = 71.5;
+let altura_max = Math.ceil(altura_m);
+let peso_min = Math.floor(peso_kg);
+let si = Number.MAX_VALUE+1 ===Number.MAX_VALUE;
