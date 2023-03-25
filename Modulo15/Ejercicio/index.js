2bb015caaf80c2b5fb34cf864907dca753d91ed0
Tiago09YT <sanyara2010@gmail.com>
2023-03-25 14:22:31 -0500

    Ejercicio15

---
+++ Modulo15/Ejercicio/index.js
@@ -0,0 +1,14 @@
+const nombre = "Santiago"
+const apellido = "Yara"
+
+const persona = {
+    nombre: nombre,
+    apellido: apellido
+}
+
+//sessionStorage.setItem("persona",JSON.stringify(persona))
+
+//localStorage.setItem("persona",JSON.stringify(persona))
+let min = new Date()
+//document.cookie = `persona = ${JSON.stringify(persona)}; expires =` + new Date(min.setMinutes(min.getMinutes()+2)).toUTCString()
+
